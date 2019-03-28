3e33b446c59cd223d2291086aa3ffa7da9a5981b
Satytan <saty_tan@mail.ru>
2019-03-28 22:45:15 +0300

    init

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace ConsoleApp1
 {
@@ -10,6 +11,10 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            // Выполнение первого упражнения
+            t1.L1.letsDoIt(args);
+
+            ReadKey();
         }
     }
 }
---
+++ ConsoleApp1/t1/L1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace ConsoleApp1.t1
+{
+    class L1
+    {
+        public static void letsDoIt(string[] args)
+        {
+            WriteLine("Task 1");
+        }
+
+    }
+}
