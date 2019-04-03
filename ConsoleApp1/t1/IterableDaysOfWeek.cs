09eec3e49b25405d3cd5d932aac2757047704e75
Satytan <saty_tan@mail.ru>
2019-04-03 22:14:22 +0300

    Sample Iterable

---
+++ ConsoleApp1/t1/IterableDaysOfWeek.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace ConsoleApp1.t1
+{
+    class IterableDaysOfWeek
+    {
+        private string[] days = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+        public IEnumerable<string> Days
+        {
+            get
+            {
+                yield return "Понедельник";
+                yield return "Вторник";
+                yield return "Среда";
+                yield return "Четверг";
+                yield return "Пятница";
+                yield return "Суббота";
+                yield return "Воскресенье";
+            }
+        }
+
+    }
+}
---
+++ ConsoleApp1/t1/IterableMonth.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ConsoleApp1.t1
+{
+    class IterableMonth : IEnumerable
+    {
+        private string[] month = { "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" };
+        public IEnumerator GetEnumerator()
+        {
+            for (int index = 0; index < month.Length; index++)
+            {
+                yield return month[index];
+            }
+        }
+    }
+}
